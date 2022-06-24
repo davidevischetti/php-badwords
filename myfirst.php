52ed068032e8c190d390f8612af1a00ec83ff2c7
Davide <davidevischetti@gmail.com>
2022-06-24 14:58:05 +0200

    modifica get per parola intera

--- myfirst.php
+++ myfirst.php
@@ -1,9 +1,9 @@
+<!-- VARIABILI PHP -->
 <?php
-$bad_word = $_GET['bad_word'];
+$bad_word =' ' . $_GET['bad_word'] . ' ';
 $text = "The Logitech G PRO X SUPERLIGHT and the Razer Viper Ultimate are both amazing wireless gaming mice. If you're a fan of ultra-light mice, the Logitech is worth considering as it's among the lightest wireless mice we've ever tested. It also has a wider CPI range and is fully compatible with Mac, which the Razer isn't. However, the Razer is still very lightweight, has two programmable buttons on each side, and feels more comfortable to use than the Logitech.";
-$new_text = str_ireplace($bad_word, '***', $text)
+$new_text = str_ireplace($bad_word, ' *** ', $text)
 ?>
-
 
 
 <!DOCTYPE html>
