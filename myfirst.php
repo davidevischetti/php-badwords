91dd84d907c970ee49b00afccb85523d0b2d3f2f
Davide <davidevischetti@gmail.com>
2022-06-24 15:44:52 +0200

    correzioni aggiuntive

--- myfirst.php
+++ myfirst.php
@@ -1,6 +1,6 @@
 <!-- VARIABILI PHP -->
 <?php
-$bad_word =' ' . $_GET['bad_word'] . ' ';
+$bad_word =$_GET['bad_word'];
 $text = "The Logitech G PRO X SUPERLIGHT and the Razer Viper Ultimate are both amazing wireless gaming mice. If you're a fan of ultra-light mice, the Logitech is worth considering as it's among the lightest wireless mice we've ever tested. It also has a wider CPI range and is fully compatible with Mac, which the Razer isn't. However, the Razer is still very lightweight, has two programmable buttons on each side, and feels more comfortable to use than the Logitech.";
 $new_text = str_ireplace($bad_word, ' *** ', $text)
 ?>
@@ -23,7 +23,7 @@ $new_text = str_ireplace($bad_word, ' *** ', $text)
         <div class="col col-s">
             <h2>TESTO ORIGINALE</h2>
             <p><?= $text?></p>
-            <h3>Lunghezza testo&colon; <?= strlen($text)?></h3> 
+            <h3>Lunghezza testo&colon; <?= strlen($text)?></h3>
         </div>
         
 
@@ -38,10 +38,15 @@ $new_text = str_ireplace($bad_word, ' *** ', $text)
         <div class="col  col-r">
             <h2>TESTO CENSURATO</h2>
             <?php
-            if ($bad_word != '')    
-            echo '<p>' . $new_text . '</p>
-            <h3>Lunghezza testo&colon;' . strlen($new_text) . '</h3>'
+            if (empty($bad_word))  {
+            echo '<p></p>
+                <h3>Lunghezza testo&colon;0 </h3>';; ;  
+            } else {
+                echo '<p>' . $new_text . '</p>
+                <h3>Lunghezza testo&colon;' . strlen($new_text) . '</h3>';; 
+            }
             ?>
+
         </div>
 
     </div>
